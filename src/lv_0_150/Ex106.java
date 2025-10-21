6bc718173cefad6351e0298d7ae6b57377a7263f
toma9512 <gmldnjs985@gmail.com>
2025-10-22 08:59:39 +0900

    lv0 : 수열과 구간 쿼리 3

---
+++ src/lv_0_150/Ex106.java
@@ -0,0 +1,30 @@
+package lv_0_150;
+
+/*
+문제 설명
+정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [i, j] 꼴입니다.
+각 query마다 순서대로 arr[i]의 값과 arr[j]의 값을 서로 바꿉니다.
+위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+1 ≤ arr의 길이 ≤ 1,000
+0 ≤ arr의 원소 ≤ 1,000,000
+1 ≤ queries의 길이 ≤ 1,000
+0 ≤ i < j < arr의 길이
+ */
+
+public class Ex106 {
+    public int[] solution(int[] arr, int[][] queries) {
+        // 바꾸기
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        // 리턴
+        return arr;
+    }
+}

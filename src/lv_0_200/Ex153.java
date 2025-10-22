505b4d3e42a6e230354a00e392eba2482bd111df
toma9512 <gmldnjs985@gmail.com>
2025-10-22 10:18:28 +0900

    lv0 : 문자열 안에 문자열

---
+++ src/lv_0_200/Ex153.java
@@ -0,0 +1,17 @@
+package lv_0_200;
+
+/*
+문제 설명
+문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ str1의 길이 ≤ 100
+1 ≤ str2의 길이 ≤ 100
+문자열은 알파벳 대문자, 소문자, 숫자로 구성되어 있습니다.
+ */
+
+public class Ex153 {
+    public int solution(String str1, String str2) {
+        return (str1.contains(str2)) ? 1 : 2;
+    }
+}

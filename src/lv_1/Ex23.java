e62123a066dcd1cf267a3d728229428357cc9cb0
toma9512 <gmldnjs985@gmail.com>
2025-10-22 13:40:12 +0900

    lv1 : 수박수박수박수박수박수?

---
+++ src/lv_1/Ex23.java
@@ -0,0 +1,27 @@
+package lv_1;
+
+/*
+문제 설명
+길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+제한 조건
+n은 길이 10,000이하인 자연수입니다.
+ */
+
+public class Ex23 {
+    public String solution(int n) {
+        // 데이터 준비하기
+        StringBuilder sb = new StringBuilder();
+
+        // 수박 추가
+        for (int i=0; i<n/2; i++) {
+            sb.append("수박");
+        }
+
+        // 홀수면 "수" 추가
+        if (n%2 == 1) sb.append("수");
+
+        // 리턴
+        return sb.toString();
+    }
+}

1a87b8af2ab17b04e5ac7e6bd19a4e545689f234
toma9512 <gmldnjs985@gmail.com>
2025-10-22 10:50:35 +0900

    lv0 : 소인수분해

---
+++ src/lv_0_200/Ex198.java
@@ -0,0 +1,36 @@
+package lv_0_200;
+
+/*
+문제 설명
+소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+2 ≤ n ≤ 10,000
+ */
+
+import java.util.*;
+
+public class Ex198 {
+    public int[] solution(int n) {
+        // 데이터 준비
+        List<Integer> list = new ArrayList<>();
+        int num = 2;
+
+        // 소인수 분해
+        while (n != 1) {
+            if (n%num == 0) {
+                list.add(num);
+                while (n%num == 0) n /= num;
+            }
+            num++;
+        }
+
+        // 배열로 변환
+        Integer[] answer = list.toArray(new Integer[list.size()]);
+
+        // 리턴
+        return Arrays.stream(answer)
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}

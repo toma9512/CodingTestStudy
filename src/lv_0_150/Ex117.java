4dc9e9f959095341b13d436328cc95749d7fb91e
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:08:59 +0900

    lv0 : 특수문자 출력하기

---
+++ src/lv_0_150/Ex117.java
@@ -0,0 +1,14 @@
+package lv_0_150;
+
+/*
+문제 설명
+다음과 같이 출력하도록 코드를 작성해 주세요.
+출력 예시
+!@#$%^&*(\'"<>?:;
+ */
+
+public class Ex117 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}

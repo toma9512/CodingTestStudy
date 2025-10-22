495dcf15dc7d08afeaedc644f5f94277496c22b7
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:22:30 +0900

    조건 문자열

---
+++ src/lv_0_150/Ex122.java
@@ -0,0 +1,34 @@
+package lv_0_150;
+
+/*
+문제 설명
+문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
+두 수가 n과 m이라면
+">", "=" : n >= m
+"<", "=" : n <= m
+">", "!" : n > m
+"<", "!" : n < m
+두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
+
+제한 사항
+1 ≤ n, m ≤ 100
+ */
+
+public class Ex122 {
+    public int solution(String ineq, String eq, int n, int m) {
+        // 데이터 담기
+        int answer = 0;
+
+        // 대소 비교
+        if (eq.equals("=")) {
+            if (ineq.equals(">")) answer = (n>=m) ? 1 : 0;
+            else answer = (n<=m) ? 1 : 0;
+        } else {
+            if (ineq.equals(">")) answer = (n>m) ? 1 : 0;
+            else answer = (n<m) ? 1 : 0;
+        }
+
+        // 리턴
+        return answer;
+    }
+}

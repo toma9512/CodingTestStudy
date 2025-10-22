5b18c65c058a974c8b9986357d6d5a00cbab935a
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:47:12 +0900

    lv0 : 문자열 출력하기

---
+++ src/lv_0_150/Ex137.java
@@ -0,0 +1,23 @@
+package lv_0_150;
+
+/*
+문제 설명
+문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+
+제한사항
+1 ≤ str의 길이 ≤ 1,000,000
+str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다.
+ */
+
+import java.util.*;
+
+public class Ex137 {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        // 출력
+        System.out.println(a);
+    }
+}

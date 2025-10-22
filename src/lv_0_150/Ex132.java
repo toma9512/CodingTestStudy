f61dcced242825155f8bea879c8f580b81827022
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:39:08 +0900

    lv0 : 전국 대회 선발 고사

---
+++ src/lv_0_150/Ex132.java
@@ -0,0 +1,48 @@
+package lv_0_150;
+
+/*
+문제 설명
+0번부터 n - 1번까지 n명의 학생 중 3명을 선발하는 전국 대회 선발 고사를 보았습니다. 등수가 높은 3명을 선발해야 하지만, 개인 사정으로 전국 대회에 참여하지 못하는 학생들이 있어 참여가 가능한 학생 중 등수가 높은 3명을 선발하기로 했습니다.
+각 학생들의 선발 고사 등수를 담은 정수 배열 rank와 전국 대회 참여 가능 여부가 담긴 boolean 배열 attendance가 매개변수로 주어집니다. 전국 대회에 선발된 학생 번호들을 등수가 높은 순서대로 각각 a, b, c번이라고 할 때 10000 × a + 100 × b + c를 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+3 ≤ rank의 길이 = attendance의 길이 ≤ 100
+rank[i]는 i번 학생의 선발 고사 등수를 의미합니다.
+rank의 원소는 1부터 n까지의 정수로 모두 서로 다릅니다.
+attendance[i]는 i번 학생의 전국 대회 참석 가능 여부를 나타냅니다.
+attendance[i]가 true라면 참석 가능, false면 참석 불가능을 의미합니다.
+attendance의 원소 중 적어도 3개는 true입니다.
+ */
+
+import java.util.*;
+
+public class Ex132 {
+    public int solution(int[] rank, boolean[] attendance) {
+        // 데이터 준비
+        Integer[] sorted = Arrays.stream(rank).boxed().toArray(Integer[]::new);
+        List<Integer> rankList = new ArrayList<>(Arrays.asList(sorted));
+        int answer = 0;
+        int count = 2;
+
+        // 정렬
+        Arrays.sort(sorted);
+
+        // 반복
+        for (Integer i : sorted) {
+            // 10000, 100, 1 준비
+            int temp = (int) Math.pow(100, count);
+
+            // true이면 더해주고 count에 1 빼주기
+            if (attendance[rankList.indexOf(i)]) {
+                answer += rankList.indexOf(i) * temp;
+                count--;
+            }
+
+            // 3개 채워지면 종료
+            if (count < 0) break;
+        }
+
+        // 리턴
+        return answer;
+    }
+}

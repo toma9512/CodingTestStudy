c524b84e0ef78230f8fd29524064d7ddf5d73163
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:23:24 +0900

    lv0 : 배열 만들기 4

---
+++ src/lv_0_150/Ex123.java
@@ -0,0 +1,37 @@
+package lv_0_150;
+
+/*
+문제 설명
+정수 배열 arr가 주어집니다. arr를 이용해 새로운 배열 stk를 만드려고 합니다.
+변수 i를 만들어 초기값을 0으로 설정한 후 i가 arr의 길이보다 작으면 다음 작업을 반복합니다.
+만약 stk가 빈 배열이라면 arr[i]를 stk에 추가하고 i에 1을 더합니다.
+stk에 원소가 있고, stk의 마지막 원소가 arr[i]보다 작으면 arr[i]를 stk의 뒤에 추가하고 i에 1을 더합니다.
+stk에 원소가 있는데 stk의 마지막 원소가 arr[i]보다 크거나 같으면 stk의 마지막 원소를 stk에서 제거합니다.
+위 작업을 마친 후 만들어진 stk를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+1 ≤ arr의 길이 ≤ 100,000
+1 ≤ arr의 원소 ≤ 100,000
+ */
+
+import java.util.*;
+
+public class Ex123 {
+    public int[] solution(int[] arr) {
+        // 데이터 준비
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+
+        // i가 arr의 길이와 같아질 때까지 반복
+        while (i<arr.length) {
+            if (stk.size() == 0) stk.add(arr[i++]);
+            else if (stk.get(stk.size()-1) >= arr[i]) stk.remove(stk.size()-1);
+            else stk.add(arr[i++]);
+        }
+
+        // 리턴
+        if (stk.size() == 0) return new int[] {-1};
+        Integer[] answer = stk.toArray(new Integer[0]);
+        return Arrays.stream(answer).mapToInt(Integer::intValue).toArray();
+    }
+}

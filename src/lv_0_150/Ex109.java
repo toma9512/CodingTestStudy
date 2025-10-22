19e71e28f859a225fa1070645e0731a028bee396
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:02:34 +0900

    lv0 : 세 개의 구분자

---
+++ src/lv_0_150/Ex109.java
@@ -0,0 +1,36 @@
+package lv_0_150;
+
+/*
+문제 설명
+임의의 문자열이 주어졌을 때 문자 "a", "b", "c"를 구분자로 사용해 문자열을 나누고자 합니다.
+예를 들어 주어진 문자열이 "baconlettucetomato"라면 나눠진 문자열 목록은 ["onlettu", "etom", "to"] 가 됩니다.
+문자열 myStr이 주어졌을 때 위 예시와 같이 "a", "b", "c"를 사용해 나눠진 문자열을 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+단, 두 구분자 사이에 다른 문자가 없을 경우에는 아무것도 저장하지 않으며, return할 배열이 빈 배열이라면 ["EMPTY"]를 return 합니다.
+
+제한사항
+1 ≤ myStr의 길이 ≤ 1,000,000
+myStr은 알파벳 소문자로 이루어진 문자열 입니다.
+ */
+
+import java.util.*;
+
+public class Ex109 {
+    public String[] solution(String myStr) {
+        // myStr에서 구분자를 " "로 변환하기
+        myStr = myStr.replace("a", " ").replace("b", " ").replace("c", " ");
+        List<String> strList = new ArrayList<>();
+
+        // " "를 기준으로 구분짓기
+        String[] arr = myStr.split(" ");
+
+        // 비어있는 것을 제외하고 리스트에 추가
+        for (String word : arr) {
+            if (!word.isEmpty()) strList.add(word);
+        }
+
+        // 리턴
+        return (strList.size() != 0) ?
+                strList.toArray(new String[strList.size()])
+                : new String[] {"EMPTY"};
+    }
+}

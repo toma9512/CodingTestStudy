942c2ef226fdbfd304cb3600bc00305348e524c8
toma9512 <gmldnjs985@gmail.com>
2025-10-22 09:34:27 +0900

    lv0 : a와 b 출력하기

---
+++ src/lv_0_150/Ex129.java
@@ -0,0 +1,24 @@
+package lv_0_150;
+
+/*
+문제 설명
+정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+제한사항
+-100,000 ≤ a, b ≤ 100,000
+ */
+
+import java.util.Scanner;
+
+public class Ex129 {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // 출력
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}

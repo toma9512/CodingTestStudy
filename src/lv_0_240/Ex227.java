5c84802aadaf371431fbe950b2c8cc0a07d35cf0
toma9512 <gmldnjs985@gmail.com>
2025-10-22 11:20:17 +0900

    lv0 : [PCCE 기출문제] 1번 / 문자

---
+++ src/lv_0_240/Ex227.java
@@ -0,0 +1,21 @@
+package lv_0_240;
+
+/*
+문제 설명
+주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+출력 예시
+3
+2
+1
+Let's go!
+ */
+
+public class Ex227 {
+    public static void main(String[] args) {
+        String message = "Let's go!"; // 빈칸
+
+        System.out.println("3\n2\n1"); // 빈칸 2개
+        System.out.println(message);
+    }
+}

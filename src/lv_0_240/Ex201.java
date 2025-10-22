ae3a0b8fed1026ab494382198f39632929da4a92
toma9512 <gmldnjs985@gmail.com>
2025-10-22 10:52:45 +0900

    lv0 : 이진수 더하기

---
+++ src/lv_0_240/Ex201.java
@@ -0,0 +1,23 @@
+package lv_0_240;
+
+/*
+문제 설명
+이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+return 값은 이진수를 의미하는 문자열입니다.
+1 ≤ bin1, bin2의 길이 ≤ 10
+bin1과 bin2는 0과 1로만 이루어져 있습니다.
+bin1과 bin2는 "0"을 제외하고 0으로 시작하지 않습니다.
+ */
+
+public class Ex201 {
+    public String solution(String bin1, String bin2) {
+        // 데이터 준비
+        int a1 = Integer.parseInt(bin1, 2);
+        int a2 = Integer.parseInt(bin2, 2);
+
+        // 리턴
+        return Integer.toBinaryString(a1+a2);
+    }
+}

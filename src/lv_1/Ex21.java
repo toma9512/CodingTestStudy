37db9da02363a0e0cd8b320ee4f9fe7ead570a3f
toma9512 <gmldnjs985@gmail.com>
2025-10-22 12:43:42 +0900

    lv1 : 제일 작은 수 제거하기

---
+++ src/lv_1/Ex21.java
@@ -0,0 +1,37 @@
+package lv_1;
+
+/*
+문제 설명
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+제한 조건
+arr은 길이 1 이상인 배열입니다.
+인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+ */
+
+import java.util.*;
+
+public class Ex21 {
+    public int[] solution(int[] arr) {
+        // 데이터 준비
+        int[] answer = new int[arr.length-1];
+        int[] sorted = Arrays.copyOf(arr, arr.length);
+        int n = 0;
+
+        // 길이가 1이면 리턴
+        if (arr.length == 1) return new int[] {-1};
+
+        // 정렬
+        Arrays.sort(sorted);
+        int min = sorted[0]; // 가장 작은값
+
+        // 데이터 담기
+        for (int i=0; i<answer.length; i++) {
+            if (arr[n] == min) n++;
+            answer[i] = arr[n++];
+        }
+
+        // 리턴
+        return answer;
+    }
+}

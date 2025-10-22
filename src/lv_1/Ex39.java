fa7b167029b8aea23f55a1e9c7269f6f250d02e1
toma9512 <gmldnjs985@gmail.com>
2025-10-22 16:25:41 +0900

    lv1 : 시저 암호

---
+++ src/lv_1/Ex39.java
@@ -0,0 +1,66 @@
+package lv_1;
+
+/*
+문제 설명
+어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+제한 조건
+공백은 아무리 밀어도 공백입니다.
+s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
+s의 길이는 8000이하입니다.
+n은 1 이상, 25이하인 자연수입니다.
+ */
+
+public class Ex39 {
+    // public String solution(String s, int n) {
+    //     // 데이터 준비
+    //     String answer = "";
+    //
+    //     // 데이터 담기
+    //     for (int i=0; i<s.length(); i++) {
+    //         // 데이터 초기화
+    //         char ch = s.charAt(i);
+    //
+    //         // 공백인 경우
+    //         if (ch == ' ') {
+    //             answer += " ";
+    //             continue;
+    //         }
+    //
+    //         // n만큼 뒤로
+    //         for (int j=0; j<n; j++) {
+    //             ch++;
+    //             if ((ch>'Z'&&ch<'a') || ch>'z') ch -= 26;
+    //         }
+    //
+    //         // 저장
+    //         answer += ch;
+    //     }
+    //
+    //     // 리턴
+    //     return answer;
+    // }
+    public String solution(String s, int n) {
+        // 데이터 준비
+        String answer = "";
+
+        // 데이터 담기
+        for (int i=0; i<s.length(); i++) {
+            // 데이터 초기화
+            char ch = s.charAt(i);
+
+            // n만큼 밀기
+            if (ch>='a' && ch<='z') {
+                ch = (char) ((ch+n-'a')%26+'a');
+            } else if (ch>='A'&&ch<='Z') {
+                ch = (char) ((ch+n-'A')%26+'A');
+            }
+
+            // 저장
+            answer += ch;
+        }
+
+        // 리턴
+        return answer;
+    }
+}

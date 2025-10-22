e3164c8200b72f1831b257b5aeff8cf0f99901e1
toma9512 <gmldnjs985@gmail.com>
2025-10-22 10:55:34 +0900

    lv0 : [PCCE 기출문제] 2번 / 피타고라스의 정리

---
+++ src/lv_0_240/Ex205.java
@@ -0,0 +1,24 @@
+package lv_0_240;
+
+/*
+문제 설명
+직각삼각형이 주어졌을 때 빗변의 제곱은 다른 두 변을 각각 제곱한 것의 합과 같습니다.
+직각삼각형의 한 변의 길이를 나타내는 정수 a와 빗변의 길이를 나타내는 정수 c가 주어질 때, 다른 한 변의 길이의 제곱, b_square 을 출력하도록 한 줄을 수정해 코드를 완성해 주세요.
+
+제한사항
+1 ≤ a < c ≤ 100
+ */
+
+import java.util.*;
+
+public class Ex205 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a; // 수정
+
+        System.out.println(b_square);
+    }
+}

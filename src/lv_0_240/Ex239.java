deea97a720659bd0cfecdf82d762620120eb8f84
toma9512 <gmldnjs985@gmail.com>
2025-10-22 11:32:27 +0900

    lv0 : 옹알이 (1)

---
+++ src/lv_0_240/Ex239.java
@@ -0,0 +1,41 @@
+package lv_0_240;
+
+/*
+문제 설명
+머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ babbling의 길이 ≤ 100
+1 ≤ babbling[i]의 길이 ≤ 15
+babbling의 각 문자열에서 "aya", "ye", "woo", "ma"는 각각 최대 한 번씩만 등장합니다.
+즉, 각 문자열의 가능한 모든 부분 문자열 중에서 "aya", "ye", "woo", "ma"가 한 번씩만 등장합니다.
+문자열은 알파벳 소문자로만 이루어져 있습니다.
+ */
+
+public class Ex239 {
+    public int solution(String[] babbling) {
+        // 데이터 준비
+        int answer = 0;
+
+        // 옹알이를 "."으로 바꾸기
+        for (int i=0; i<babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", ".")
+                    .replace("ye", ".")
+                    .replace("woo", ".")
+                    .replace("ma", ".");
+
+            // 전부 .인지 확인
+            boolean check = true;
+            for (int j=0; j<babbling[i].length(); j++) {
+                if (babbling[i].charAt(j) != '.') {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) answer++;
+        }
+
+        // 리턴
+        return answer;
+    }
+}
